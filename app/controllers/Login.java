17dac9f3a4fcb27a829fe2b3d617caa3c8aaf7a9
Rahul <rahulj51@gmail.com>
2012-01-02 23:46:30 +0800

    Added loginUser method

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.User;
 import play.mvc.Controller;
+import actions.LoginAction;
 
 public class Login extends Controller {
 	
@@ -10,6 +11,26 @@ public class Login extends Controller {
     }	
     
     public static void loginUser(String email, String password) {    
+    	
+    	LoginAction loginAction = new LoginAction();
+    	try {
+    		User user = loginAction.login(email, password);
+    		
+            session.put("user.fullName", user.fullName);
+            session.put("user.email", user.email);
+            session.put("user.id", user.id);
+            flash.success("Welcome, " + user.fullName);    	
+            
+            render("Login/success.html");
+            
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		flash.error(e.getMessage());
+            params.flash();
+            validation.keep(); 
+            index(email, password);    		
+    	}
+    	
     	
     }
 
