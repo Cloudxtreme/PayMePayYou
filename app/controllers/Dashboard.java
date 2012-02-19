5cce3484e10435d2d5002a5137852fb22baef8b8
Rahul <rahulj51@gmail.com>
2012-02-19 18:25:14 +0800

    Added logic for switching between pools

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -8,7 +8,7 @@ import play.mvc.Controller;
 public class Dashboard extends Controller {
 
 	
-    public static void index(long userId, long selectedPoolId) {
+    public static void index(long userId, long selectedPoolId) { //TODO rjain9 we shouldn't be passing userid here.
     	
     	User user = User.findById(userId);
     	ExpensePool selectedPool = ExpensePool.findById(selectedPoolId);
